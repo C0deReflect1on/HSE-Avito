a084bf0559c1bc269caacbe7c69d7404a98b4372
C0deReflect1on <antonexperience@gmail.com>
2025-09-29 18:13:23 +0300

    hw_2_task_1

---
+++ Algorithms/homework_2/test_task_1.py
@@ -0,0 +1,20 @@
+import pytest
+from task_1 import main
+
+@pytest.mark.parametrize("pushed,poped,expected", [
+    (["1","2","3"], ["2","1","3"], True),
+    (["1","2","3"], ["3","2","1"], True),
+    (["1","2","3"], ["3","1","2"], False),
+    (["1"], ["1"], True),
+    (["1","2"], ["2","1"], True),
+    (["1","2"], ["1","2"], True),
+    (["1","2","3","4"], ["4","3","2","1"], True),
+    (["1","2","3","4"], ["2","1","4","3"], True),
+])
+def test_main(monkeypatch, pushed, poped, expected):
+    inputs = iter([
+        " ".join(pushed), 
+        " ".join(poped)
+    ])
+    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    assert main() == expected
